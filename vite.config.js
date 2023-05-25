dbe35345fddd3337e09fcd0a36b845f79944fc06
Lyris Nunes <lyrisnunesdasilva@gmail.com>
2023-05-25 13:11:28 -0300

    novo

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+module.exports = {
+  build: {
+    rollupOptions: {
+      external: ['react-icons/fi']
+    }
+  }
+};
+
